fa1a8ceb2db9a49ce3c2b400bf507acff802de65
weekiatscis <weekiat.tan.2023@scis.smu.edu.sg>
2025-03-29 00:27:28 +0800

    idk man

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/singapore-journey-planner/' : '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ESD_UI/'  // Replace with your actual repository name
+    : '/',
   lintOnSave: false,
   devServer: {
     historyApiFallback: true,
